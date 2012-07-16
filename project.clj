d2ffaf2950d522df645ea3eb527e2de0460182d6
James Reeves <jreeves@weavejester.com>
2012-07-16 19:39:16 +0100

    Updated project details

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ring-json "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Ring middleware for handling JSON"
+  :url "https://github.com/ring-clojure/ring-json"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [cheshire "4.0.1"]
                  [ring/ring-core "1.1.1"]])
