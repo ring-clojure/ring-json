7d8d446ee4a257ab4aefd1e7ec6d6495e0969601
James Reeves <jreeves@weavejester.com>
2012-07-16 19:22:56 +0100

    Added code and tests from wrap-json-params

--- src/ring/middleware/json.clj
+++ src/ring/middleware/json.clj
@@ -2,6 +2,24 @@
   (:use ring.util.response)
   (:require [cheshire.core :as json]))
 
+(defn- json-request? [request]
+  (if-let [type (:content-type request)]
+    (not (empty? (re-find #"^application/(vnd.+)?json" type)))))
+
+(defn wrap-json-params
+  "Middleware that converts request bodies in JSON format to a map of
+  parameters, which is added to the request map on the :json-params and
+  :params keys."
+  [handler]
+  (fn [request]
+    (if-let [body (and (json-request? request) (:body request))]
+      (let [params (json/parse-string (slurp body))]
+        (handler
+         (-> request
+             (assoc :json-params params)
+             (update-in [:params] merge params))))
+      (handler request))))
+
 (defn wrap-json-response
   "Middleware that converts responses with a map for a body into a JSON
   response."
--- test/ring/middleware/test/json.clj
+++ test/ring/middleware/test/json.clj
@@ -1,6 +1,34 @@
 (ns ring.middleware.test.json
   (:use ring.middleware.json
-        clojure.test))
+        clojure.test
+        ring.util.io))
+
+(deftest test-json-params
+  (let [handler  (wrap-json-params identity)]
+    (testing "xml body"
+      (let [request  {:content-type "application/xml"
+                      :body (string-input-stream "<xml></xml>")
+                      :params {"id" 3}}
+            response (handler request)]
+        (is (= "<xml></xml>") (slurp (:body response)))
+        (is (= {"id" 3} (:params response)))
+        (is (nil? (:json-params response)))))
+
+    (testing "json body"
+      (let [request  {:content-type "application/json; charset=UTF-8"
+                      :body (string-input-stream "{\"foo\": \"bar\"}")
+                      :params {"id" 3}}
+            response (handler request)]
+        (is (= {"id" 3, "foo" "bar"} (:params response)))
+        (is (= {"foo" "bar"} (:json-params response)))))
+
+    (testing "custom json body"
+      (let [request  {:content-type "application/vnd.foobar+json; charset=UTF-8"
+                      :body (string-input-stream "{\"foo\": \"bar\"}")
+                      :params {"id" 3}}
+            response (handler request)]
+        (is (= {"id" 3, "foo" "bar"} (:params response)))
+        (is (= {"foo" "bar"} (:json-params response)))))))
 
 (deftest test-json-response
   (let [handler  (constantly {:status 200 :headers {} :body {:foo "bar"}})
