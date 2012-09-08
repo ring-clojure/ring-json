ac4b4ab64f06ccf8c4525245af764064d10bfd37
James Reeves <jreeves@weavejester.com>
2012-09-08 19:29:12 +0100

    Added test profiles for Clojure 1.3 and 1.4

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [cheshire "4.0.2"]
-                 [ring/ring-core "1.1.5"]])
+                 [ring/ring-core "1.1.5"]]
+  :profiles
+  {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
