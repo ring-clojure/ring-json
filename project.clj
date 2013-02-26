a64311e20287d2217447807a745be44355296f7c
James Reeves <jreeves@weavejester.com>
2013-02-26 15:00:51 +0000

    Added Clojure 1.5.0 profile

--- project.clj
+++ project.clj
@@ -8,4 +8,5 @@
                  [ring/ring-core "1.1.8"]]
   :profiles
   {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
+   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC16"]]}})
