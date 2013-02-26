6ccbc2c381e6112e97858e40b956bd812216788e
James Reeves <jreeves@weavejester.com>
2013-02-26 14:59:02 +0000

    Updated version of ring-core

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [cheshire "5.0.2"]
-                 [ring/ring-core "1.1.5"]]
+                 [ring/ring-core "1.1.8"]]
   :profiles
   {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
