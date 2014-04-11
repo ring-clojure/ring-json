99ffa50e1a028923f7a44b0e902be1df322065de
James Reeves <jreeves@weavejester.com>
2014-04-11 18:06:09 +0100

    Added Clojure 1.6 test profile

--- project.clj
+++ project.clj
@@ -8,4 +8,5 @@
                  [ring/ring-core "1.2.2"]]
   :profiles
   {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
