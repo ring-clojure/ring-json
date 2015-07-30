95357193775b79ba91941a8d278febf0c5a3591c
James Reeves <jreeves@weavejester.com>
2015-07-30 15:42:18 +0100

    Add test profile for Clojure 1.7

--- project.clj
+++ project.clj
@@ -10,4 +10,5 @@
   :profiles
   {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
