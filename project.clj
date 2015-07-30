183bdd97bbcc0f7d7e3ae329453a999290a50ee3
James Reeves <jreeves@weavejester.com>
2015-07-30 15:52:24 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,10 @@
   :url "https://github.com/ring-clojure/ring-json"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [cheshire "5.3.1"]
-                 [ring/ring-core "1.2.2"]]
-  :plugins [[codox "0.8.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "5.5.0"]
+                 [ring/ring-core "1.4.0"]]
+  :plugins [[codox "0.8.13"]]
   :profiles
-  {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+  {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
