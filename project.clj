caec9aa773495acc3d30c0473d8a334069143a4a
James Reeves <jreeves@weavejester.com>
2019-08-07 13:43:30 +0100

    Drop support for Clojure 1.6 and below

--- project.clj
+++ project.clj
@@ -3,14 +3,12 @@
   :url "https://github.com/ring-clojure/ring-json"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.7.1"]
                  [ring/ring-core "1.6.2"]]
   :plugins [[codox "0.8.13"]]
-  :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9:+1.10" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10" "test"]}
   :profiles
-  {:1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
+  {:1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}})
